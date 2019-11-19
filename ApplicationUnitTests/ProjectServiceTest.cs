7f6cd8f73845600b6635dde994228e8e667614b5
samirhabibi <samir-habibi1@hotmail.com>
2019-11-19 14:16:17 +0100

    added unit test

---
+++ ApplicationUnitTests/ProjectServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using Application.Exceptions;
+using Application.Models.Entities;
+using Application.Repositories;
+using Application.Services;
+using Application.Utility.ClientLibrary;
+using Moq;
+using Xunit;
+
+namespace ApplicationUnitTests
+{
+    public class ProjectServiceTest
+    {
+        [Fact]
+        public void Create_ProjectIsNull_ThrowsInvalidProject()
+        {
+            // Arrange
+            var projectRepository = new Mock<IProjectRepository>();
+            var client = new Mock<IClient>();
+            projectRepository.Setup(m => m.Create(It.IsAny<Project>())).Returns((Project)null);
+            var projectService = new ProjectService(projectRepository.Object, client.Object);
+
+            // Act
+            // Assert
+            Assert.ThrowsAsync<InvalidProject>(() => projectService.Create(new Project()));
+        }
+
+        [Fact]
+        public void Update_ProjectIsNull_ThrowsInvalidProject()
+        {
+            // Arrange
+            var projectRepository = new Mock<IProjectRepository>();
+            var client = new Mock<IClient>();
+            var projectService = new ProjectService(projectRepository.Object, client.Object);
+
+            // Act
+            // Assert
+            Assert.Throws<InvalidProject>(() => projectService.Update((Project)null));
+        }
+
+
+        [Fact]
+        public void Update_GetById_ReturnsUpdateProject()
+        {
+            // Arrange
+            var projectRepository = new Mock<IProjectRepository>();
+            var client = new Mock<IClient>();
+            projectRepository.Setup(m => m.GetById(It.IsAny<string>())).Returns(new Project() { Id = "1123423523" });
+            projectRepository.Setup(m => m.Update(It.IsAny<string>(), It.IsAny<Project>()));
+            var projectService = new ProjectService(projectRepository.Object, client.Object);
+
+            // Act
+            projectService.Update(new Project() { Id = "1123423523" });
+            // Assert
+
+        }
+
+
+
+
+
+    }
+}
